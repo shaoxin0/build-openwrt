c53769f1ec5d17785c86089bdc74c07202c3f4a6
shaoxin0 <shaoxinfu0@gmail.com>
2024-09-21 20:23:18 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -7,7 +7,13 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git packages https://github.com/immortalwrt/packages' feeds.conf.default
+# sed -i '$a src-git luci https://github.com/immortalwrt/luci' feeds.conf.default
+# sed -i '$a src-git routing https://github.com/openwrt/routing' feeds.conf.default
+# sed -i '$a src-git telephony https://github.com/openwrt/telephony' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
